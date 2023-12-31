5adedf3956a7155633c7b574e384d7d952839386
omaxfernandes <max.sfernandes@outlook.com>
2023-12-30 23:13:07 -0300

    initial commit

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
+  `,
+  styleUrl: './housing-location.component.scss'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+
+}
